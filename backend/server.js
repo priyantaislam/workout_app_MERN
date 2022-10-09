f01e016779f3079c0b55234a9aa4815a50bfb35b
Priyanta Islam <priyantaislam@gmail.com>
2022-10-09 15:35:51 +1100

    added backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
 require('dotenv').config()
 const express = require('express')
-
+const workoutRoutes = require('./routes/workouts')
+const mongoose = require('mongoose')
 //express app
 const app = express()
 
-app.get('/', (req,res) => {
-  res.json({msg: 'Welcome to the app'})
+//middleware
+app.use(express.json())
+
+app.use((req,res,next) => {
+  console.log(req.path,req.method)
+  next()
 })
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port 4000')
-})
+app.use('/api/workouts',workoutRoutes)
+
+//connect to db
+
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to db & listening on port 4000')
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  }) 
+
+
 
