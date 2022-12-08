935991022545d217e1e8aa8e10f6b685e3fa960e
Priyanta Islam <islam.priyanta@gmail.com>
2022-12-08 19:53:10 +1100

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages and components
+import Home from './pages/Home'
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
-        <h1>A header</h1>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="pages"> 
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            />
+          </Routes>
+
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
