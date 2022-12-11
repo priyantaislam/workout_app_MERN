c323608145c120d5268f8e5ac27519f5515ab30a
Priyanta Islam <islam.priyanta@gmail.com>
2022-12-11 19:37:08 +1100

    added frontend cards

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
+
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  })
+
+  return (
+      <div className="home">
+          <div className="workouts">
+            {workouts && workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout}/>
+            ))}
+          </div>
+      </div>
+  )
 }
 
 export default Home
