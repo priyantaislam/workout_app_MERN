66c2f30c532e5a18c7187f21f66c76f4b315d60a
Priyanta Islam <islam.priyanta@gmail.com>
2023-04-09 00:01:41 +1000

    fixed html bug in Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,16 +14,16 @@ const Login = () => {
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log in</h3>
       <label>Email:</label>
-      <input>
+      <input
         type="email"
         onChange={(e) => setEmail(e.target.value)}
         value={email}
-      </input>
-      <input>
+      />
+      <input
         type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
-      </input>
+      />
       <button>Login</button>
 
     </form>
