126b1e2e89a2c9a467d09a64aea9775fd6777fa9
Priyanta Islam <islam.priyanta@gmail.com>
2023-04-13 23:32:22 +1000

    implementing backend auth

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,9 @@
 const requireAuth = (req,res,next) => {
 
+  //verify authentication
+  const { authorization } = req.headers
+
+  if(!authorization) {
+    return res.status(401).json({error: 'Authorization Token Required'})
+  }
 }
